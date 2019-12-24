ed1ddb97aa347a6edd7099b57b5d0cb5d15386d3
Ramani Sandip <RamaniSandip@JohnDeere.com>
2019-12-24 18:16:18 +0530

    Microservice working demo

---
+++ AccountsServer/src/main/java/com/example/demo/AccountsServer.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@Import(AccountsConfiguration.class)
+public class AccountsServer {
+
+	@Autowired
+	protected AccountRepository accountRepository;
+
+	protected Logger logger = Logger.getLogger(AccountsServer.class.getName());
+		
+
+	/**
+	 * Run the application using Spring Boot and an embedded servlet engine.
+	 * 
+	 * @param args
+	 *            Program arguments - ignored.
+	 */
+	public static void main(String[] args) {
+		// Tell server to look for accounts-server.properties or
+		// accounts-server.yml
+		System.setProperty("spring.config.name", "accounts-server");
+
+		SpringApplication.run(AccountsServer.class, args);
+	}
+}
---
+++ WebServer/src/main/java/com/example/demo/WebServerApplication.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(useDefaultFilters = false)
+public class WebServerApplication {
+
+	public static final String ACCOUNTS_SERVICE_URL = "http://accounts-service";
+	
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "web-server");
+		SpringApplication.run(WebServerApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	/**
+	 * The AccountService encapsulates the interaction with the micro-service.
+	 * 
+	 * @return A new service instance.
+	 */
+	@Bean
+	public WebAccountsService accountsService() {
+		return new WebAccountsService(ACCOUNTS_SERVICE_URL);
+	}
+
+	/**
+	 * Create the controller, passing it the {@link WebAccountsService} to use.
+	 * 
+	 * @return
+	 */
+	@Bean
+	public WebAccountsController accountsController() {
+		return new WebAccountsController(accountsService());
+	}
+
+	@Bean
+	public HomeController homeController() {
+		return new HomeController();
+	}
+}
